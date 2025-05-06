b135b892993119b57bdecc5fd29d3887da2b8781
waffobean <kjkhosra@gmail.com>
2025-05-06 08:19:58 -0400

    Added basic Choco VTuber frontend

---
+++ client.js
@@ -0,0 +1,12 @@
+L2Dwidget.init({
+  model: {
+    jsonPath: "https://choco1w1.model3.json", 
+  },
+  display: {
+    position: "right",
+    width: 200,
+    height: 400,
+  },
+  mobile: { show: true },
+  react: { opacityDefault: 1, opacityOnHover: 1 }
+});
